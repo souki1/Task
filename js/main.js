6e97057325428a92fd4f8a3b504d814dcdf5366c
souki1 <soukigirishkumar22@gmail.com>
2021-06-02 23:09:36 +0530

    rework

--- js/main.js
+++ js/main.js
@@ -1,27 +1,70 @@
 
 
+var count1=false
+var count2 =false
+var count3=false;
+var count4=false;
+var count5=false;
 
 
-let count = 0;
-let sub = document.getElementById('submit');
+var sub = document.getElementById('submit')
+sub.disabled = true;
 
-sub.disabled = true
-function fun(e){
+function fun(){
+    count1=true;
+    enable()
+    console.log(count1)
     
-    if(e==1){
-        count++;
-    }
-    else{
-        count++;
-    }
-     if(count>=5){
-        sub.disabled = false
-          sub.classList.remove('btn-secondary')
-          sub.classList.add('btn-primary')
-    }
-console.log(count);
+}
+
+function fun2(){
+    count2=true
+    enable()
+    console.log(count2)
 
 }
+function fun3(){
+    count3=true;
+  enable()
+    console.log(count3)
+
+}
+function fun4(){
+    count4=true;
+    enable()
+    console.log(count4)
+
+}
+function fun5(){
+    count5=true;
+    enable()
+    console.log(count5)
+}
+
+
+    function enable(){
+
+        if(count1 && count2 && count3 && count4 && count5){
+            sub.disabled=false
+            sub.classList.remove('btn-secondary')
+            sub.classList.add('btn-primary')
+        }
+    }
+
+    
+
+
+//console.log(count)
+
+
+
+
+
+
+
+
+
+
     //console.log(count )
         //  sub.disabled = false
         //  sub.classList.remove('btn-outline-secondary')
